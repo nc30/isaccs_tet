17fe95880740eabad6a36ea14045514cc0864193
Shoji Imamura <imamura@xshell.io>
2018-02-08 15:54:18 +0900

    ff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from __future__ import print_function
-import time
+import datetime
+import sys
 
-print("HELLO!")
-print("ffs")
+sys.stdout("hello\r\n")
+sys.stdout.write("this is {:%Y-%m-%d %H:%M:%S}\r\n".format(datetime.datetime.now()))
+sys.stdout("this is stdout message.\r\n")
+sys.stderr("this is stderr message.\r\n")
+sys.stdout("i go exit. goodbye.")
+
