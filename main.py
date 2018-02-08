adce9b03dc5c8b01276dd9db6853283ee53c6c1a
Shoji Imamura <imamura@xshell.io>
2018-02-08 16:03:04 +0900

    fff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sys.stdout.write("hello\r\n")
 sys.stdout.write("this is {:%Y-%m-%d %H:%M:%S}\r\n".format(datetime.datetime.now()))
 sys.stdout.write("environs.\r\n")
 for i in os.environ.items():
-    sys.stdout.write("{0}: '{1}'\r\n".format(*i)
+    sys.stdout.write("{0}: '{1}'\r\n".format(*i))
 
 sys.stdout.write("this is stdout message.\r\n")
 sys.stderr.write("this is stderr message.\r\n")
