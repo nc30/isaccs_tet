a22551d084064da037439479df0d6a364520e44e
Shoji Imamura <imamura@xshell.io>
2018-02-08 15:55:03 +0900

    yes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import datetime
 import sys
 
-sys.stdout("hello\r\n")
+sys.stdout.write("hello\r\n")
 sys.stdout.write("this is {:%Y-%m-%d %H:%M:%S}\r\n".format(datetime.datetime.now()))
 sys.stdout("this is stdout message.\r\n")
 sys.stderr("this is stderr message.\r\n")
